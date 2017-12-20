9c133de98eee04fa611cfd8cc6380db9347324bf
Jim Kang <jimkang@gmail.com>
2017-12-20 08:54:22 -0500

    Support arbitrary properties in cell defs.

--- apply-def-to-cell.js
+++ apply-def-to-cell.js
@@ -1,7 +1,12 @@
+var cloneDeep = require('lodash.clonedeep');
+var curry = require('lodash.curry');
+
 function applyDefToCell(def, cell) {
-  cell.type = def.type;
-  cell.rules = def.rules;
-  cell.mapSymbol = def.mapSymbol;
+  Object.getOwnPropertyNames(def).forEach(curry(copyProp)(def, cell));
+}
+
+function copyProp(source, target, prop) {
+  target[prop] = cloneDeep(source[prop]);
 }
 
 module.exports = applyDefToCell;
